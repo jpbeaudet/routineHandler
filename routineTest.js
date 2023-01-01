fbd7a8544a7215b14e609e88ab254114dbfe7d22
Jean-Philippe Beaudet <jpbeaudet@users.noreply.github.com>
2023-01-01 15:00:41 -0500

    Update routineTest.js

--- routineTest.js
+++ routineTest.js
@@ -258,3 +258,62 @@ test('addRoutine should throw an error if called with a non-Routine object', () 
   const promise = new Promise();
   expect(() => promise.addRoutine({})).toThrow('`routine` must be an instance of `Routine`');
 });
+// Additional Tests
+
+test('then should return the Routine object, allowing for chaining', () => {
+  const routine = new Routine({ evaluate: () => true });
+  expect(routine.then(mockSuccessHandler)).toBe(routine);
+});
+
+test('catch should return the Routine object, allowing for chaining', () => {
+  const routine = new Routine({ evaluate: () => false });
+  expect(routine.catch(mockFailureHandler)).toBe(routine);
+});
+
+test('addSubRoutine should return the Routine object, allowing for chaining', () => {
+  const routine = new Routine({ evaluate: () => true });
+  const subRoutine = new SubRoutine({ evaluate: () => true });
+  expect(routine.addSubRoutine(subRoutine)).toBe(routine);
+});
+// Additional tests
+
+// Routine Class
+
+test('execute should call the execute method of added subroutines, even if the evaluator function returns true', () => {
+  mockEvaluator.evaluate.mockReturnValue(true);
+  const routine = new Routine(mockEvaluator);
+  const subRoutine = new SubRoutine({ evaluate: () => true });
+  const spy = jest.spyOn(subRoutine, 'execute');
+  routine.addSubRoutine(subRoutine);
+  routine.execute();
+  expect(spy).toHaveBeenCalled();
+});
+
+test('addSubRoutine should throw an error if called with a non-Routine object', () => {
+  const routine = new Routine(mockEvaluator);
+  expect(() => routine.addSubRoutine({})).toThrow('`routine` must be an instance of `Routine`');
+});
+
+// SubRoutine Class
+
+test('then should add a success handler to the successHandlers array', () => {
+  const subRoutine = new SubRoutine(mockEvaluator);
+  subRoutine.then(mockSuccessHandler);
+  expect(subRoutine.successHandlers).toEqual([mockSuccessHandler]);
+});
+
+test('catch should add a failure handler to the failureHandlers array', () => {
+  const subRoutine = new SubRoutine(mockEvaluator);
+  subRoutine.catch(mockFailureHandler);
+  expect(subRoutine.failureHandlers).toEqual([mockFailureHandler]);
+});
+
+test('then should throw an error if called with a non-function', () => {
+  const subRoutine = new SubRoutine(mockEvaluator);
+  expect(() => subRoutine.then('not a function')).toThrow('`successHandler` must be a function');
+});
+
+test('catch should throw an error if called with a non-function', () => {
+  const subRoutine = new SubRoutine(mockEvaluator);
+  expect(() => subRoutine.catch('not a function')).toThrow('`failureHandler` must be a function');
+});
